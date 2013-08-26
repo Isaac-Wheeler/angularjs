9f61c5194289c6cd17cfb93eb103421375bebab2
Wayne Moorefield <wayne.moorefield@gmail.com>
2013-08-26 14:29:59 -0400

    Adding example code for AngularJS

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,24 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope', function($scope) {
+  	$scope.names = [
+  		{
+  			'first': 'Tim',
+  			'last': 'Mote'
+  		},
+  		{
+  			'first': 'Dan',
+  			'last': 'Bean'
+  		},
+  		{
+  			'first': 'Dennis',
+  			'last': 'Mote'
+  		}
+  	];
 
+  	// default oder
+  	$scope.orderProp = 'first';
   }])
   .controller('MyCtrl2', [function() {
 
