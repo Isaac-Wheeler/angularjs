f4d54341187f2362f7111549c869fb050f17043e
Isaac Wheeler <isaacwheeler@mage-tech.org>
2013-10-15 09:53:26 -0400

    Added Views 4-6 to the index menu and app.js

--- app/js/app.js
+++ app/js/app.js
@@ -7,5 +7,8 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
+    $routeProvider.when('/view4', {templateUrl: 'partials/partial4.html', controller: 'MyCtrl4'});
+    $routeProvider.when('/view5', {templateUrl: 'partials/partial5.html', controller: 'MyCtrl5'});
+    $routeProvider.when('/view6', {templateUrl: 'partials/partial6.html', controller: 'MyCtrl6'});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
