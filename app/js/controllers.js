00bf5d845a2e2f4f51dbe9412fba9f806fe4264b
Isaac Wheeler <isaacwheeler@mage-tech.org>
2013-11-04 16:29:47 -0500

    partial 2 fix

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,7 +25,44 @@ angular.module('myApp.controllers', []).
   		// default oder
   		$scope.orderProp = 'first';
   	}])
-  	.controller('MyCtrl4', ['$scope', function ($scope) {
+  	.controller('MyCtrl2', ['$scope', function ($scope) {
+  		$scope.todos = [
+  			{
+  				'todoItem': 'finish this test app',
+  				'done': false
+  			},
+  			{
+  				'todoItem': 'come up with more todos',
+  				'done': false
+  			}
+  		];
+
+  		$scope.returnTotalTodos = function () {
+  			return $scope.todos.length;
+   		}
+  		$scope.addNewTodo = function () {
+  			if ($scope.newTodoText.length) {
+  				// Add new todo to list
+  				$scope.todos.push({
+  					'todoItem': $scope.newTodoText,
+  					'done': false
+  				});
+  				// reset the input box
+  				$scope.newTodoText = '';
+  			}
+  		}
+
+  		$scope.clearFinishedTodos = function () {
+  			var newTodos = [];
+  			$scope.todos.forEach(function (todo) {
+  				if (!todo.done) {
+  					newTodos.push(todo);
+  				}
+  			});
+  			$scope.todos = newTodos;
+  		}
+ 	}]);
+    .controller('MyCtrl4', ['$scope', function ($scope) {
   		$scope.contacts = [
   			{
   				'name': 'joe',
@@ -105,9 +142,7 @@ angular.module('myApp.controllers', []).
                 } else if ($scope.math.indexOf("/") != -1) {
                 $scope.math = $scope.math.split("/")
                 $scope.math =($scope.math[0] + "/" + $scope.math[1] + "=" + (parseInt($scope.math[0]) / parseInt($scope.math[1])) );
-                }
-                
-                
+                }                
                 
   				}
 
